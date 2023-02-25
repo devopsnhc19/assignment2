fa82f8f6d887817fdce931a9bfd985f368e03e61
devopsnhc19 <126224107+devopsnhc19@users.noreply.github.com>
2023-02-25 13:13:14 +0530

    First Commit

---
+++ add.java
@@ -0,0 +1,11 @@
+class add
+{
+     public static void main(String args[])
+    {
+      int l=10,b=20;
+      if(l<b)
+               System.out.print(b+" is larger than"+l);
+   else
+               System.out.print("l is larger than b");
+   }
+}
